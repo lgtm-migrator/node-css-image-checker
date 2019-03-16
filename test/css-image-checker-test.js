eb26fdb5e5afb34639fc4efbca47dc649b6925ec
Henrik Gemal <henrik@gemal.dk>
2019-03-16 14:55:42 +0100

    test

--- test/css-image-checker-test.js
+++ test/css-image-checker-test.js
@@ -1,16 +1,2 @@
 "use strict";
 
-const expect = require('chai').expect;
-const cmd = require('./cmd');
-
-describe('The pizza CLI', () => {
-  it('should print the correct error', async () => {
-    try {
-      await cmd.execute('css-image-checker', ['--sausage']);
-    } catch(err) {
-      expect(err.trim()).to.equal(
-        '  Invalid option --sausage'
-      );
-    }
-  });
-});
