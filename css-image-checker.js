a8d63245c1911b55791649933f180dccef31c6a1
Henrik Gemal <henrik@gemal.dk>
2019-03-16 12:52:02 +0100

    better

--- css-image-checker.js
+++ css-image-checker.js
@@ -1,81 +1,81 @@
 #!/usr/bin/env node
 
+"use strict";
+
 const recursive = require("recursive-readdir");
 const optimist = require('optimist');
 const parseCssUrls = require('css-url-parser');
 const fs = require('fs');
 const path = require('path');
 const isurl = require('is-url');
-const folder = optimist.argv["folder"];
+const folder = optimist.argv['folder'];
+const verbose = optimist.argv['verbose'];
 
 const app = {
-   init: function(folder) {
-     that = this;
-     if (folder) {
-        if (fs.existsSync(folder)) {
-           var stats = fs.statSync(folder);
-           if (stats.isDirectory()) {
-             that.checkFolder(folder);
-           } else {
-             console.log("Oops! Not a folder: " + folder);
-             return false;
-           }
+    init: function(folder, verbose) {
+        var that = this;
+        if (folder) {
+            if (fs.existsSync(folder)) {
+                var stats = fs.statSync(folder);
+                if (stats.isDirectory()) {
+                    var err = that.checkFolder(folder, verbose);
+                    return err;
+                } else {
+                    console.log("Oops! Folder is not a real folder: " + folder);
+                    return -1;
+                }
+            } else {
+                console.log("Oops! Folder does not exists: " + folder);
+                return -2;
+            }
         } else {
-           console.log("Oops! Folder does not exists: " + folder);
-           return false;
+            console.log("Oops! Please specify a folder");
+            return -3;
         }
-     } else {
-        console.log("Oops! Please specify a folder");
-        return false;
-     }
-   },
-   checkFolder: function(folder){
-     that = this;
-     errors = 0;
-     recursive(folder, function (err, files) {
-      files.forEach(function(file) {
-         var ext = path.extname(file)
-         if (ext === '.css') {
-             //console.log('file: ' + file);
-             filePath = path.dirname(file) + path.sep;
-             //console.log('filepath: ' + filePath);
-             filecontent = fs.readFileSync(file, {encoding: 'utf-8'});
-            var cssUrls = parseCssUrls(filecontent);
-            //console.log('cssurls: ' + cssUrls);
-            cssUrls.forEach(function(cssUrl) {
-               if (isurl(cssUrl)) {
-               } else {
-                  cssReal = cssUrl.replace(/(\?|#).*$/, "");
-                  if (cssReal.charAt(0) === '/') {
-                  //console.log('From:' + cssUrl + ' to:' + cssReal);
-                     fullPath = folder + cssReal;
-                  } else {
-                     fullPath = filePath + cssReal;
-                  }
-                  //console.log(fullPath);
-                  //fullPath = path.resolve(filePath, cssReal);
-                  //console.log(fullPath);
-                  //console.log('fullpath: ' + fullPath);
-                  if (!fs.existsSync(fullPath)) {
-                     console.log('ERROR');
-                     console.log('Not found: ' + fullPath);
-                     console.log('Not found: ' + cssUrl);
-                     console.log('Not found: ' + cssReal);
-                     console.log('In file: ' + file);
-                     console.log();
-                     errors++;
-                  } else {
-                     //console.log('OK: ' + fullPath);
-                  }
-               }
+    },
+    checkFolder: function(folder, verbose){
+        var that = this;
+        var errors = 0;
+        recursive(folder, function(err, files) {
+            files.forEach(function (file) {
+                var ext = path.extname(file)
+                if (ext === '.css') {
+                    var filePath = path.dirname(file) + path.sep;
+                    var filecontent = fs.readFileSync(file, {encoding: 'utf-8'});
+                    var cssUrls = parseCssUrls(filecontent);
+                    cssUrls.forEach(function (cssUrl) {
+                        if (isurl(cssUrl)) {
+                        } else {
+                            var cssReal = cssUrl.replace(/(\?|#).*$/, "");
+                            var fullPath = filePath + cssReal;
+                            if (cssReal.charAt(0) === '/') {
+                                fullPath = folder + cssReal;
+                            }
+                            if (!fs.existsSync(fullPath)) {
+                                console.log('Error found in: ' + file);
+                                console.log('Full path not found: ' + fullPath);
+                                console.log('Path in CSS file: ' + cssUrl);
+                                if (cssUrl !== cssReal) {
+                                    console.log('Original path in CSS file: ' + cssReal);
+                                }
+                                console.log();
+                                errors++;
+                            } else {
+                                if (verbose) {
+                                    console.log('OK: ' + fullPath);
+                                }
+                            }
+                        }
+                    })
+                }
             })
-         }
-       })
-       console.log("Number of errors: " + errors);
-     });
-   }
+        });
+        console.log("Number of errors: " + errors);
+        return errors;
+    }
 }
-//init the app module with postCount
-app.init(folder);
+
+var err = app.init(folder, verbose);
+process.exitCode = err
 
 module.exports = app;
