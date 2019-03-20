ffb760918d7faac52eb3e53992352b7230316cab
Henrik Gemal <henrik@gemal.dk>
2019-03-20 11:42:22 +0100

    Add tests (#1)
    * Add tests
    
    * fix tests
    
    * try it
    
    * fix
    
    * test
    
    * more tests
    
    * fix
    
    * try it out
    
    * fix
    
    * fix
    
    * test
    
    * fix

--- test/index.js
+++ test/index.js
@@ -3,9 +3,10 @@
 const assert = require('assert');
 const path = require('path');
 const spawn = require('child_process').spawn;
-var expect = require('chai').expect;
+const expect = require('chai').expect;
 
 describe('index.js', function () {
+
     this.timeout(8000);
 
     it('should exit 1 having css problems', function (done) {
@@ -24,17 +25,98 @@ describe('index.js', function () {
         });
     });
 
-    it('should exit 1 having css problems', function (done) {
+    it('should exit 0 having no css problems with url params ?', function (done) {
         var out = '';
         spawn('node', [path.join(__dirname, '../index.js'), '--folder', 'test/css2'], {
             cwd: path.join(__dirname, '../'),
         }).on('exit', function (code) {
             assert.equal(code, 0);
-            console.log(out.toString());
+            expect(out).to.match(/Number of errors: 0/);
+            done();
+        }).stdout.on('data', function (data) {
+            out += data;
+        });
+    });
+
+    it('should exit 0 having no css problems with verbose', function (done) {
+        var out = '';
+        spawn('node', [path.join(__dirname, '../index.js'), '--verbose', '--folder', 'test/css2'], {
+            cwd: path.join(__dirname, '../'),
+        }).on('exit', function (code) {
+            assert.equal(code, 0);
+            expect(out).to.match(/OK: .*?\.\.\/firefox\.png/);
+            expect(out).to.match(/Number of errors: 0/);
+            done();
+        }).stdout.on('data', function (data) {
+            out += data;
+        });
+    });
+
+    it('should exit 0 having no css problems without url params', function (done) {
+        var out = '';
+        spawn('node', [path.join(__dirname, '../index.js'), '--folder', 'test/css3'], {
+            cwd: path.join(__dirname, '../'),
+        }).on('exit', function (code) {
+            assert.equal(code, 0);
+            expect(out).to.match(/Number of errors: 0/);
+            done();
+        }).stdout.on('data', function (data) {
+            out += data;
+        });
+    });
+
+    it('should exit 0 having css problems with url params #', function (done) {
+        var out = '';
+        spawn('node', [path.join(__dirname, '../index.js'), '--folder', 'test/css4'], {
+            cwd: path.join(__dirname, '../'),
+        }).on('exit', function (code) {
+            assert.equal(code, 0);
+            expect(out).to.match(/Number of errors: 0/);
+            done();
+        }).stdout.on('data', function (data) {
+            out += data;
+        });
+    });
+
+    it('should exit 0 having no css problems absolute and url params', function (done) {
+        var out = '';
+        spawn('node', [path.join(__dirname, '../index.js'), '--folder', 'test/css5'], {
+            cwd: path.join(__dirname, '../'),
+        }).on('exit', function (code) {
+            assert.equal(code, 0);
+            expect(out).to.match(/Number of errors: 0/);
+            done();
+        }).stdout.on('data', function (data) {
+            out += data;
+        });
+    });
+
+    it('should exit 1 having css problems absolute', function (done) {
+        var out = '';
+        spawn('node', [path.join(__dirname, '../index.js'), '--folder', 'test/css6'], {
+            cwd: path.join(__dirname, '../'),
+        }).on('exit', function (code) {
+            assert.equal(code, 1);
             expect(out).to.match(/Error found in:.*?style\.css/);
-            expect(out).to.match(/Full path not found.*?\.\.\/img\/404\.png/);
-            expect(out).to.match(/Path in CSS file: \.\.\/img\/404\.png\?v=5/);
-            expect(out).to.match(/Original path in CSS file: \.\.\/img\/404\.png/);
+            expect(out).to.match(/Full path not found: test\/css6\/404\/firefox\.png/);
+            expect(out).to.match(/Path in CSS file: \/404\/firefox\.png\?\#iefix/);
+            expect(out).to.match(/Original path in CSS file: \/404\/firefox\.png/);
+            expect(out).to.match(/Full path not found: test\/css6\/40\/firefox\.png/);
+            expect(out).to.match(/Path in CSS file: \/40\/firefox\.png/);
+            expect(out).to.match(/Number of errors: 2/);
+            done();
+        }).stdout.on('data', function (data) {
+            out += data;
+        });
+    });
+
+    it('should exit 0 having css problems url', function (done) {
+        var out = '';
+        spawn('node', [path.join(__dirname, '../index.js'), '--folder', 'test/css7'], {
+            cwd: path.join(__dirname, '../'),
+        }).on('exit', function (code) {
+            assert.equal(code, 0);
+            expect(out).to.match(/Number of errors: 0/);
             done();
         }).stdout.on('data', function (data) {
             out += data;
@@ -47,7 +129,6 @@ describe('index.js', function () {
         }).on('exit', function (code) {
             assert.equal(code, 2);
         }).stdout.on('data', function (data) {
-            //console.log('data:' + data.toString());
             assert.equal(data.toString(), 'Oops! Please specify a folder\n');
             done();
         });
@@ -64,7 +145,7 @@ describe('index.js', function () {
         });
     });
 
-    it('should exit 4 if folder does not exist', function (done) {
+    it('should exit 4 if folder is not a folder', function (done) {
         spawn('node', [path.join(__dirname, '../index.js'), '--folder', 'test/index.js'], {
             cwd: path.join(__dirname, '../'),
         }).on('exit', function (code) {
@@ -74,5 +155,4 @@ describe('index.js', function () {
             done();
         });
     });
-
 });
